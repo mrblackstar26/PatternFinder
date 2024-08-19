887edd255fe4429df3854f97de596850ed181bca
Uday Patel <93428115+mrblackstar26@users.noreply.github.com>
2024-08-19 13:21:48 +0530

    Update emptyfile.py

--- emptyfile.py
+++ emptyfile.py
@@ -13,9 +13,15 @@ def check_empty_jsp_files(jsp_list_file, main_code_dir, empty_files_output):
     for jsp_file in jsp_files_to_check:
         jsp_file_path = os.path.join(main_code_dir, jsp_file)
         
-        # Check if the file exists and is empty
-        if os.path.exists(jsp_file_path) and os.path.getsize(jsp_file_path) == 0:
-            empty_jsp_files.append(jsp_file)
+        # Check if the file exists and read its content
+        if os.path.exists(jsp_file_path):
+            with open(jsp_file_path, 'r', encoding='utf-8') as file:
+                content = file.read().strip()
+                # Check if the content is empty
+                if not content:
+                    empty_jsp_files.append(jsp_file)
+        else:
+            print(f"File not found: {jsp_file_path}")
 
     # Write the names of the empty JSP files to the output .txt file
     with open(empty_files_output, 'w') as output_file:
